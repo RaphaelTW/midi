58d8f69ebfc61d4bbca8db2d0d7b771e4581cfc6
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2022-11-16 22:43:39 -0300

    versão 3 -  aqui foi adicionado os sons e trocado o while para o for, assim facilitando o código e deixando mais curto

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-function tocaSom(idElementAudio) {
-    document.querySelector(idElementAudio).play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
-    //template string
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
-        tocaSom('idAudio');
-    };
+        tocaSom(idAudio);
+    }
 
-    contador = contador + 1;
 }
