b1d7a6bebd057b358d46c685edd5d2a65b146a73
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2022-11-16 22:15:42 -0300

    versão 2 - criando funções para o som

---
+++ main.js
@@ -0,0 +1,22 @@
+function tocaSom(idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom('idAudio');
+    };
+
+    contador = contador + 1;
+}
